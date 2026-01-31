1e940078f326ebefdd56a3ecfefa14cc2406bade
Ivan Mineev <H.E.L.P.M.E@yandex.ru>
2026-01-31 15:10:55 +0500

    feat: set up login logic

--- app/src/main/java/io/github/he11pme/thousandscourses/fragments/login/LoginFragment.kt
+++ app/src/main/java/io/github/he11pme/thousandscourses/fragments/login/LoginFragment.kt
@@ -1,23 +1,23 @@
 package io.github.he11pme.thousandscourses.fragments.login
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import io.github.he11pme.thousandscourses.R
 import io.github.he11pme.thousandscourses.databinding.FragmentLoginBinding
+import kotlinx.coroutines.launch
 
 class LoginFragment : Fragment() {
 
     private lateinit var binding: FragmentLoginBinding
     private val viewModel: LoginViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,6 +25,53 @@ class LoginFragment : Fragment() {
     ): View {
         binding = FragmentLoginBinding.inflate(layoutInflater, container, false)
 
+        bindToViewModel()
+
         return binding.root
+    }
+
+    private fun bindToViewModel() {
+        binding.viewModel = viewModel
+        bindState()
+        bindAction()
+    }
+
+    private fun bindState() {
+        viewModel.state.observe(viewLifecycleOwner) {
+            binding.loginBtn.isEnabled = it is LoginViewModel.State.CorrectInput
+        }
+    }
+
+    private fun bindAction() {
+        lifecycleScope.launch {
+            viewModel.action.collect {
+                when (it) {
+                    LoginViewModel.Action.Login -> navigateToHomeFragment()
+                    LoginViewModel.Action.ToOk -> openOk()
+                    LoginViewModel.Action.ToVk -> openVk()
+                }
+            }
+        }
+    }
+
+    private fun navigateToHomeFragment() {
+        findNavController().navigate(R.id.navigateFromLoginFragmentToHomeFragment)
+    }
+
+    private fun openOk() = openLink(OK_URI)
+
+    private fun openVk() = openLink(VK_URI)
+
+    fun openLink(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW).apply {
+            data = Uri.parse(url)
+        }
+
+        requireContext().startActivity(intent)
+    }
+
+    companion object {
+        private const val VK_URI = "https://vk.com/"
+        private const val OK_URI = "https://ok.ru/"
     }
 }
--- app/src/main/java/io/github/he11pme/thousandscourses/fragments/login/LoginViewModel.kt
+++ app/src/main/java/io/github/he11pme/thousandscourses/fragments/login/LoginViewModel.kt
@@ -1,7 +1,85 @@
 package io.github.he11pme.thousandscourses.fragments.login
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.launch
 
 class LoginViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    val email = MutableLiveData<String>()
+    val password = MutableLiveData<String>()
+    private val _state = MutableLiveData<State>()
+    val state: LiveData<State> get() = _state
+
+    private val _action = MutableSharedFlow<Action>()
+    val action: Flow<Action> get() = _action
+
+    init {
+        _state.value = State.Input
+
+        observeInput()
+    }
+
+    private fun observeInput() {
+        email.observeForever { validateInput() }
+        password.observeForever { validateInput() }
+    }
+
+    private fun validateInput() {
+        val emailValue = email.value.orEmpty()
+        val passwordValue = password.value.orEmpty()
+
+        val emailValid = isEmailValid(emailValue)
+        val passwordValid = isPasswordValid(passwordValue)
+
+        _state.value = if (emailValid && passwordValid) State.CorrectInput
+        else State.Input
+    }
+
+    private fun isEmailValid(email: String): Boolean {
+        return EMAIL_REGEX.matches(email)
+    }
+
+    private fun isPasswordValid(password: String): Boolean {
+        return password.isNotEmpty()
+    }
+
+    fun onClickLoginBtn() = navigateToHome()
+
+    private fun navigateToHome() {
+        viewModelScope.launch { _action.emit(Action.Login) }
+    }
+
+    fun onClickVkBtn() = navigateToVk()
+
+    private fun navigateToVk() {
+        viewModelScope.launch { _action.emit(Action.ToVk) }
+    }
+
+    fun onCLickOkBtn() = navigateToOk()
+
+    private fun navigateToOk() {
+        viewModelScope.launch { _action.emit(Action.ToOk) }
+    }
+
+    sealed interface State {
+        object Input : State
+        object CorrectInput : State
+    }
+
+    sealed interface Action {
+        object ToVk : Action
+        object ToOk : Action
+        object Login : Action
+    }
+
+    companion object {
+        private val EMAIL_REGEX = Regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$")
+
+    }
+
 }
